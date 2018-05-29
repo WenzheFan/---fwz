89903f31e9c247d38f4c521e7748e4cc091ecb7d
fanwenzhe <523935688@qq.com>
2018-05-29 15:34:34 +0800

    简单的计时器

--- 计时器/ViewController.m
+++ 计时器/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSTimer * timerRepeat;
+    int secCount;
+}
+@property (strong, nonatomic) IBOutlet UIButton *codeBtn;
 
 @end
 
@@ -18,7 +22,33 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (IBAction)button:(id)sender {
+    if (timerRepeat == nil) {
+        
+        timerRepeat = [NSTimer scheduledTimerWithTimeInterval: 1.0f
+                                                       target: self
+                                                     selector: @selector(repeatRefresh:)
+                                                     userInfo: nil
+                                                      repeats: YES];
+        
+        secCount = 59;
+    }
+    [_codeBtn setTitle:@"(60)秒后重发" forState:UIControlStateNormal];
+    [_codeBtn setEnabled:NO];
+}
+- (void) repeatRefresh:(NSTimer *)timer{
+    if(secCount==1){
+        [_codeBtn setTitle:@"获取验证码" forState:UIControlStateNormal];
+        [_codeBtn setEnabled:YES];
+        [timerRepeat invalidate];
+        timerRepeat = nil;
+        return;
+    }
+    NSString *curstr = [NSString stringWithFormat:@"(%ld)秒后重发", (long)secCount];
+    
+    [_codeBtn setTitle:curstr forState:UIControlStateNormal];
+    secCount--;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
